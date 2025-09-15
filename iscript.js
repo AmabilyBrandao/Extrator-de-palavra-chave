46ca057446f8dffcdd5d226664db95556cfcc74c
AmabilyBrandao <amabily.brandao@escola.pr.gov.br>
2025-09-15 08:40:08 -0300

    ghhhh

--- iscript.js
+++ iscript.js
@@ -8,6 +8,18 @@ campoResultado.textContent = palavrasChave.joinC(",");
 }
 function processaTexto(texto){
      let palavras = texto.split(/\P{L}+/u);
+   
+     let de frequencias = {};
+     for(let i in plavras){
+          frequencias[i] = 0;
+          for (let j of palavras) {
+             if (i == j){
+                 frequencia[i]++;
+              }
+          }
+     }
+
+     consolo.log(requencias);
 
      return palavras;
 }
