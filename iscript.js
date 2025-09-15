053f26347a5be2ae53758048e1b0cd94672417e4
AmabilyBrandao <amabily.brandao@escola.pr.gov.br>
2025-09-15 08:49:05 -0300

    hhhh

--- iscript.js
+++ iscript.js
@@ -1,11 +1,17 @@
-const botaMostraPalavras = document.querySelector("#botao-palavrachave"):
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
-function mostraPalavrasChave(){
- const texto = document.querySelector ("#entrada-de-texto").value;
- const campoResultado = document.querySelector ("#resultado-palayrachave"):
- const palavras Chave = processa Texte(texto):
-campoResultado.textContent = palavrasChave.joinC(",");
+
+function mostraPalavrasChave() {
+  const texto = document.querySelector("#entrada-de-texto").value;
+
+  const campoResultado = document.querySelector("#resultado-palavrachave");
+
+  const palavrasChave = processaTexto(texto);
+
+  campoResultado.textContent = palavrasChave.join(", ");
 }
+
 function processaTexto(texto){
      let palavras = texto.split(/\P{L}+/u);
    
